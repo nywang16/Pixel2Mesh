34fa436adb632ba1d1f106eecbf192387c872d5c
Nanyang Wang <nywang16@fudan.edu.cn>
2019-01-11 10:26:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(name='pixel2mesh',
 	author_email='nywang16@163.com',
 	url='http://bigvid.fudan.edu.cn/pixel2mesh',
 	download_url='https://github.com/nywang16/Pixel2Mesh',
-	license='MIT',
 	install_requires=['numpy', 'tflearn', 'opencv-python'],
 	package_data={'pixel2mesh': ['README.md']},
 	packages=find_packages())
